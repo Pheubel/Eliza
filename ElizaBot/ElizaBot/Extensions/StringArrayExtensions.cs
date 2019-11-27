74e850e383ae86403897c12f9d55e03587010d0a
Pheubel <luca.guagliardo@live.nl>
2019-11-27 23:20:01 +0100

    Create StringArrayExtensions.cs

---
+++ ElizaBot/ElizaBot/Extensions/StringArrayExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElizaBot.Extensions
+{
+    public static class StringArrayExtensions
+    {
+        public static string[] ToUpper(this string[] array)
+        {
+            var upperArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                upperArray[i] = array[i].ToUpper();
+            }
+
+            return upperArray;
+        }
+
+        public static string[] ToLower(this string[] array)
+        {
+            var lowerArray = new string[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                lowerArray[i] = array[i].ToLower();
+            }
+
+            return lowerArray;
+        }
+    }
+}
