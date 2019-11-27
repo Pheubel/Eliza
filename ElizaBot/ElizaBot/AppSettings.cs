08e6ead82ef02e5ad600ca2992a74b84440318df
Pheubel <luca.guagliardo@live.nl>
2019-11-27 20:19:14 +0100

    Revert "Revert "Added settings""
    This reverts commit 5146cb26c036dbb67d8015f022be122316f548b8.

---
+++ ElizaBot/ElizaBot/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ElizaBot
+{
+    /// <summary> The settings to apply to the client.</summary>
+    public class AppSettings
+    {
+        /// <summary> The token the bot will use for authentication.</summary>
+        public string BotToken { get; set; }
+        /// <summary> The connectionstring used to connect to database of the bot.</summary>
+        public string ConnectionString { get; set; }
+        /// <summary> The character the bot should respond to.</summary>
+        public string Prefix { get; set; }
+    }
+}
--- ElizaBot/ElizaBot/Program.cs
+++ ElizaBot/ElizaBot/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using DiscordAbstraction;
+using DiscordAbstraction.Extensions;
+using DiscordAbstraction.Interfaces;
+using ElizaBot.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace ElizaBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main() => await BuildService().RunAsync();
+
+        private static IClientService BuildService()
         {
-            Console.WriteLine("Hello World!");
+            var settings = new AppSettingsLoader().LoadSettings<AppSettings>();
+
+            return ClientServiceBuilder.CreateDefaultBuilder()
+                .UseToken(settings.BotToken)
+                .AddSingletonService<RNGService>()
+                .AddSingletonService(settings)
+                .UseEventHandler(options =>
+                {
+                    options.UseCommandsWithPrefix(settings.Prefix);
+                }).Build();
         }
     }
 }
