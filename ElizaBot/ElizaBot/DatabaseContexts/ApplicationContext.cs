2566d804288b60272c0c393eb061285a5475bd8e
Pheubel <luca.guagliardo@live.nl>
2019-11-27 23:31:18 +0100

    made tagname unique

--- ElizaBot/ElizaBot/DatabaseContexts/ApplicationContext.cs
+++ ElizaBot/ElizaBot/DatabaseContexts/ApplicationContext.cs
@@ -15,5 +15,15 @@ namespace ElizaBot.DatabaseContexts
 
         public DbSet<User> Users { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tag>(entity =>
+            {
+                entity.HasIndex(tag => tag.TagName).IsUnique();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
