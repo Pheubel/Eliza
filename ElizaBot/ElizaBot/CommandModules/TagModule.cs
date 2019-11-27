9382aee4d360caa0719c8646652a6b9a4ddfbdfa
Pheubel <luca.guagliardo@live.nl>
2019-11-27 20:36:09 +0100

    Set-up TagModule

---
+++ ElizaBot/ElizaBot/CommandModules/TagModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElizaBot.CommandModules
+{
+    public class TagModule : ModuleBase
+    {
+        [Command("tag")]
+        public async Task Tag(params string[] tags)
+        {
+
+        }
+
+        [Command("subscribe")]
+        public async Task Subscribe(params string[] tags)
+        {
+
+        }
+
+        [Command("unsubscribe")]
+        public async Task Unsubscribe(params string[] tags)
+        {
+
+        }
+    }
+}
