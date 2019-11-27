7684931a82b66bfd240aa1c7d7edcaca08db4ac6
Pheubel <luca.guagliardo@live.nl>
2019-11-27 22:43:16 +0100

    Create ApplicationContext.cs

---
+++ ElizaBot/ElizaBot/DatabaseContexts/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using ElizaBot.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElizaBot.DatabaseContexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
