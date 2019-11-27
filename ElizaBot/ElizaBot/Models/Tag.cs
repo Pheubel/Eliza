4bb0966c2157e8bbe0ea0560328349261406a8e4
Pheubel <luca.guagliardo@live.nl>
2019-11-27 22:42:59 +0100

    Added database models

---
+++ ElizaBot/ElizaBot/Models/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ElizaBot.Models
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TagName { get; set; }
+
+        [InverseProperty(nameof(User.SubscibedTags))]
+        public List<User> Subscribers { get; set; }
+        [InverseProperty(nameof(User.BlacklistedTags))]
+        public List<User> Blacklisters { get; set; }
+    }
+}
---
+++ ElizaBot/ElizaBot/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ElizaBot.Models
+{
+    public class User
+    {
+        [Key]
+        public ulong UserId { get; set; }
+
+        [InverseProperty(nameof(Tag.Subscribers))]
+        public List<Tag> SubscibedTags { get; set; }
+        [InverseProperty(nameof(Tag.Blacklisters))]
+        public List<Tag> BlacklistedTags { get; set; }
+    }
+}
