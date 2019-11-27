65cfb8e97de5b07067932c2eae0100fb17adb691
Pheubel <luca.guagliardo@live.nl>
2019-11-27 23:20:37 +0100

    created tag command code

--- ElizaBot/ElizaBot/CommandModules/TagModule.cs
+++ ElizaBot/ElizaBot/CommandModules/TagModule.cs
@@ -1,7 +1,10 @@
 ﻿using Discord.Commands;
 using ElizaBot.DatabaseContexts;
+using ElizaBot.Extensions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +22,27 @@ namespace ElizaBot.CommandModules
         [Command("tag")]
         public async Task Tag(params string[] tags)
         {
+            if (tags.Length == 0)
+                return;
 
+            var sanitizedTags = tags.ToLower();
+
+            var usersToTag = await _context.Users
+                .AsNoTracking()
+                .Where(user => !user.BlacklistedTags.Any(t => sanitizedTags.Contains(t.TagName)) && user.SubscribedTags.All(t => sanitizedTags.Contains(t.TagName)))
+                .ToArrayAsync();
+
+            if (usersToTag.Length == 0)
+                return;
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < usersToTag.Length; i++)
+            {
+                var user = await Context.Guild.GetUserAsync(usersToTag[i].UserId);
+                builder.Append(user.Mention);
+            }
+
+            await ReplyAsync(builder.ToString());
         }
 
         [Command("subscribe")]
