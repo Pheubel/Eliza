e4d12bad04e3530a34ec79b4d8f975986a2085a4
Pheubel <luca.guagliardo@live.nl>
2019-11-28 20:34:18 +0100

    Update ApplicationContext.cs

--- ElizaBot/ElizaBot/DatabaseContexts/ApplicationContext.cs
+++ ElizaBot/ElizaBot/DatabaseContexts/ApplicationContext.cs
@@ -9,15 +9,39 @@ namespace ElizaBot.DatabaseContexts
 {
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext(DbContextOptions options) : base(options)
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Tag> Tags { get; set; }
 
+        public DbSet<UserSubcribedTag> UserSubscribedTags { get; set; }
+        public DbSet<UserBlacklistedTag> UserBlacklistedTags { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserSubcribedTag>(entity =>
+            {
+                entity.HasKey(ut => new { ut.UserId, ut.TagId });
+
+                entity.HasOne(ut => ut.Tag).WithMany(t => t.Subscribers).HasForeignKey(t => t.TagId);
+                entity.HasOne(ut => ut.User).WithMany(t => t.SubscribedTags).HasForeignKey(t => t.UserId);
+            });
+
+            modelBuilder.Entity<UserBlacklistedTag>(entity =>
+            {
+                entity.HasKey(ut => new { ut.UserId, ut.TagId });
+
+                entity.HasOne(ut => ut.Tag).WithMany(t => t.Blacklisters).HasForeignKey(t => t.TagId);
+                entity.HasOne(ut => ut.User).WithMany(t => t.BlacklistedTags).HasForeignKey(t => t.UserId);
+            });
+
+
             modelBuilder.Entity<Tag>(entity =>
             {
                 entity.HasIndex(tag => tag.TagName).IsUnique();
