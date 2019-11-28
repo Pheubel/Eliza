1b09bdad18049773997b4e128ec061692cf3e158
Pheubel <luca.guagliardo@live.nl>
2019-11-28 20:34:36 +0100

    Create ApplicationContextFactory.cs

---
+++ ElizaBot/ElizaBot/DatabaseContexts/DesignTimeFactories/ApplicationContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ElizaBot.DatabaseContexts.DesignTimeFactories
+{
+    public class ApplicationContextFactory : IDesignTimeDbContextFactory<ApplicationContext>
+    {
+        public ApplicationContext CreateDbContext(string[] args)
+        {
+            var settings = new AppSettingsLoader().LoadSettings<AppSettings>();
+            var builder = new DbContextOptionsBuilder<ApplicationContext>();
+            builder.UseSqlite(settings.ConnectionString);
+
+            return new ApplicationContext(builder.Options);
+        }
+    }
+}
