d928a5c428af9ca9e073e8f7408ea1ceedc0a343
Pheubel <luca.guagliardo@live.nl>
2019-12-19 14:37:02 +0100

    implemented tag interface

---
+++ ElizaBot/ElizaWebClient/Shared/Models/TagMetaData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElizaWebClient.Shared.Models
+{
+    public class TagMetaData
+    {
+        public string TagName { get; set; }
+        public int SubscriberCount { get; set; }
+        public int BlacklisterCount { get; set; }
+    }
+}
