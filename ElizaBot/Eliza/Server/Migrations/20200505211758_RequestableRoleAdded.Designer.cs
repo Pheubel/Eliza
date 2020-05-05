773dcb06b09288349d145ae10e07f79200bee9b5
Pheubel <luca.guagliardo@live.nl>
2020-05-05 23:29:53 +0200

    added migration for requestablerole

--- ElizaBot/Eliza/Server/Migrations/20200505211758_RequestableRoleAdded.Designer.cs
+++ ElizaBot/Eliza/Server/Migrations/20200505211758_RequestableRoleAdded.Designer.cs
@@ -2,14 +2,16 @@
 using Eliza.Database.DatabaseContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace ElizaBot.Migrations
+namespace Eliza.Server.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200505211758_RequestableRoleAdded")]
+    partial class RequestableRoleAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ElizaBot/Eliza/Server/Migrations/20200505211758_RequestableRoleAdded.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eliza.Server.Migrations
+{
+    public partial class RequestableRoleAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RequestableRoles",
+                columns: table => new
+                {
+                    RoleId = table.Column<ulong>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RoleName = table.Column<string>(maxLength: 32, nullable: false),
+                    GuildId = table.Column<ulong>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestableRoles", x => x.RoleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RequestableRoles");
+        }
+    }
+}
