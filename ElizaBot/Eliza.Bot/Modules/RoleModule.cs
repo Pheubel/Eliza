10af14805e0dc0260c57b6a6d6325ddf302f6e7d
Pheubel <luca.guagliardo@live.nl>
2020-05-05 19:28:41 +0200

    added role module set up

---
+++ ElizaBot/Eliza.Bot/Modules/RoleModule.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eliza.Bot.Modules
+{
+    public class RoleModule : ModuleBase
+    {
+        [Command("give role")]
+        public async Task GiveRoleAsync(IRole role)
+        {
+            if (!(Context.User is SocketGuildUser user))
+                return;
+            await user.AddRoleAsync(role);
+        }
+
+        [Command("take role")]
+        public async Task TakeRoleAsync(IRole role)
+        {
+            if (!(Context.User is SocketGuildUser user))
+                return;
+            await user.RemoveRoleAsync(role);
+        }
+    }
+}
