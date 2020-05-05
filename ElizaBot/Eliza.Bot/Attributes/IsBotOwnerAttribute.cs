bc4b19e622cdd345b7bcf1d9c0f840131a9ba28d
Pheubel <luca.guagliardo@live.nl>
2020-05-05 22:54:41 +0200

    Create IsBotOwnerAttribute.cs

---
+++ ElizaBot/Eliza.Bot/Attributes/IsBotOwnerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Eliza.Bot.Attributes
+{
+    public class IsBotOwnerAttribute : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            if (context.User.Id == services.GetRequiredService<BotSettings>().OwnerId)
+                return Task.FromResult(PreconditionResult.FromSuccess());
+
+            return Task.FromResult(PreconditionResult.FromError("You are not allowed to use this command."));
+        }
+    }
+}
