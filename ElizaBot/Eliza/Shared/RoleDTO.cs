c48a63a933886dfe39ac470bf00e306e840c9642
Pheubel <luca.guagliardo@live.nl>
2020-05-06 10:27:56 +0200

    added role DTO's for data transmission

---
+++ ElizaBot/Eliza/Shared/RoleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Eliza.Shared
+{
+    public class RoleDTO
+    {
+        public ulong RoleId { get; set; }
+        public ulong GuildId { get; set; }
+    }
+}
---
+++ ElizaBot/Eliza/Shared/RoleWithNameDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Eliza.Shared
+{
+    public class RoleWithNameDTO
+    {
+        public ulong RoleId { get; set; }
+        public ulong GuildId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
