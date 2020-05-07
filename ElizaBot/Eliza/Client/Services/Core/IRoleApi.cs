8028b62f427fe4a584b17b0caaa13384544b3405
Pheubel <luca.guagliardo@live.nl>
2020-05-07 08:45:58 +0200

    Create IRoleApi.cs

---
+++ ElizaBot/Eliza/Client/Services/Core/IRoleApi.cs
@@ -0,0 +1,17 @@
+﻿using Eliza.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Eliza.Client.Services.Core
+{
+    public interface IRoleApi
+    {
+        Task<IEnumerable<RoleDTO>> GetDiscordRolesAsync(ulong guildId);
+        Task<IEnumerable<RoleDTO>> GetUserDiscordRolesAsync(ulong guildId, ulong userId);
+        Task<IEnumerable<ulong>> GetRequestableRoleIdsAsync(ulong guildId);
+        Task GiveRoleAsync(ulong guildId, ulong roleId);
+        Task TakeRoleAsync(ulong guildId, ulong roleId);
+        Task SetRequestableAsync(ulong guildId, ulong roleId);
+        Task UnsetRequestableAsync(ulong guildId, ulong roleId);
+    }
+}
