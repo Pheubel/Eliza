3f4c9519c178c7e573c220725ee476bda022992f
Pheubel <luca.guagliardo@live.nl>
2020-08-28 15:30:21 +0200

    added stuff

--- ElizaBot/Eliza/Shared/Constants.cs
+++ ElizaBot/Eliza/Shared/Constants.cs
@@ -7,5 +7,6 @@ namespace Eliza.Shared
     public static class Constants
     {
         public const string IsBotOwner = nameof(IsBotOwner);
+        public const ulong RozServerId = 206238980583325696;
     }
 }
