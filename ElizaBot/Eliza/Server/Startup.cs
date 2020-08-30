dbc6de95ea38373b780bdfc3f1c44607f94d2963
Pheubel <luca.guagliardo@live.nl>
2020-08-30 04:15:46 +0200

    added forward headers

--- ElizaBot/Eliza/Server/Startup.cs
+++ ElizaBot/Eliza/Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -121,6 +122,11 @@ namespace Eliza.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseClientRateLimiting();
             app.UseIpRateLimiting();
